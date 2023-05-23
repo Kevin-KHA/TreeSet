aabb93a189f8cf7077ed0ae92a865e445de25c75
kevin-kha11 <kevin.hzalvarado@gmail.com>
2023-05-22 21:20:32 -0600

    comentarios explicativos

--- Alumno.java
+++ Alumno.java
@@ -1,7 +1,9 @@
 public class Alumno implements Comparable <Alumno>{
-
+    //ATRIBUTOS
     private String name;
     private int id;
+
+    //CONSTRUCTOR PARA RECEPCION DE DATOS
     public Alumno(int id, String name) {
         this.id=id;
         this.name=name;
@@ -18,6 +20,8 @@ public class Alumno implements Comparable <Alumno>{
     public void setId(int id) {
         this.id = id;
     }
+
+    //ESTE METODO ES NECESARIO PARA QUE EL TREESET FUNCIONE, ORDENA DATOS SEGUN LAS MATRICULAS
     @Override
     public int compareTo(Alumno alumno) {
         return  this.id -  alumno.getId();
--- Principal.java
+++ Principal.java
@@ -10,6 +10,7 @@ public class Principal
     public static void menu(){
         Scanner leer = new Scanner(System.in);
         int opcion = 0;
+        //treeset de tipo alumnos
         TreeSet<Alumno> datos = new TreeSet<Alumno>();
 
         do {
@@ -17,7 +18,7 @@ public class Principal
             System.out.print("Opcion elegida: ");
 
             opcion = leer.nextInt();
-            
+            //menu sencillo que le pasa a cada metodo toda la lista de datos
             switch (opcion) {
                 case 1:
                     System.out.println("----------REGISTRO DE ALUMNO--------");
@@ -46,13 +47,14 @@ public class Principal
         idIngresado = leer.nextInt();
         System.out.print(">>>Nombre: ");
         nombreIngresado = leer.next();
+        //registro de alumnos mediante constructor y lo guardamos en el treeset
         datos.add(new Alumno(idIngresado, nombreIngresado));
 
     }
 
     public static void modificar(TreeSet<Alumno> datos){
         Scanner leer = new Scanner(System.in);
-
+        //for para imprimir datos y se pueda ver que matricula deseamos buscar
         for (Alumno alumno : datos) {
             System.out.println("Matricula: " + alumno.getId() + " "+ "Nombre: " + alumno.getName());
         }
@@ -61,17 +63,17 @@ public class Principal
         System.out.print("Matricula a buscar: ");
         matriculaBuscar = leer.nextInt();
         String nombreNuevo;
+        //foreach para buscar matricula ingresada
         for (Alumno alumno : datos) {
+            //if para buscar matricula
             if(matriculaBuscar==alumno.getId()){
                 System.out.println("Matricula encontrada");
                 System.out.print("Ingrese el nuevo nombre: ");
                 nombreNuevo = leer.next();
+                //aqui se hace el reemplazo del dato que corresponde a la matricula ingresada
                 alumno.setName(nombreNuevo);
                 System.out.print("Datos actualizados correctamente");
             }   
         }
-
-
     }
-    
 }
